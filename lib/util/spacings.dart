290fd58bf74160b9f48392c43b7261053b4c7cab
sauttets <tobias.sautter@gmail.com>
2024-05-26 00:36:58 +0200

    definded util files

---
+++ lib/util/spacings.dart
@@ -0,0 +1,7 @@
+class Spacings {
+  static const double horizontal = 12.0;
+  static const double vertical = 12.0;
+  static const double cornerRadius = 12.0;
+  static const double textFontSize = 16.0;
+  static const double titleFontSize = 18.0;
+}
