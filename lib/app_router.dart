7e7553819183e7cdb97973c4fa46b6d4035020be
sauttets <tobias.sautter@gmail.com>
2024-06-18 12:22:27 +0200

    created basic login page

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:vinoveritas/src/features/general_feature/widgets/wine_card.dart';
 import 'package:vinoveritas/src/screens/settings_page.dart';
 import 'package:vinoveritas/src/screens/lexikon_page.dart';
+import 'package:vinoveritas/src/screens/startup_page.dart';
 
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => placeholderPage('WhateverPage'),
+      builder: (context, state) => const LoginScreen(),
     ),
     GoRoute(
       path: '/home',
-      builder: (context, state) => placeholderPage('HomePage'),
+      builder: (context, state) => WineListPage(wineEntries: wineEntries),
     ),
     GoRoute(
       path: '/page1',
@@ -38,3 +40,22 @@ Widget placeholderPage(String pageName) {
     ),
   );
 }
+
+
+final wineEntries = [
+    WineEntry(
+      year: '2019',
+      name: 'Châteauneuf-du-Pape',
+      volume: '850ml',
+      price: '42,00€',
+      imagePath: 'assets/images/wine.png',
+    ),
+    WineEntry(
+      year: '2019',
+      name: 'Château Migraine',
+      volume: '750ml',
+      price: '13,37€',
+      imagePath: 'assets/images/wine.png',
+    ),
+    // Add more entries as needed
+  ];
