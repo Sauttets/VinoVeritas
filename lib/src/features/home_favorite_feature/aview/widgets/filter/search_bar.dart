f5e8aedf9780dfe4664a2971f056964f9166d85b
sauttets <tobias.sautter@gmail.com>
2024-07-30 15:58:16 +0200

    updated app icon, updated release apk

--- lib/src/features/home_favorite_feature/aview/widgets/filter/search_bar.dart
+++ lib/src/features/home_favorite_feature/aview/widgets/filter/search_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vinoveritas/util/app_colors.dart';
 import 'package:vinoveritas/util/spacings.dart';
